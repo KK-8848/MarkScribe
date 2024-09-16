ea423b89966a44a74667a7daea59a63971b24964
KK-8848 <karthikkrishna8848@gmail.com>
2024-09-16 15:13:56 +0530

    Fixed errors

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get("/exercise-2", (req, res) => {
     res.redirect("/exercise-2-redirected");
 })
 app.get("/exercise-2-redirected", (req, res) => {
-    res.send("/exercise-2.ejs")
+    res.render('exercise-2.ejs')
 })
 app.listen(port, () => { console.log("Server is running") })
