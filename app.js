c71eceeda028969f8357b7ac2d0e430371c0659b
KK-8848 <karthikkrishna8848@gmail.com>
2024-09-16 13:41:01 +0530

    Initial commit to sub branch

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ app.set('view engine', 'ejs');
 app.get("/", (req, res) => {
     res.render('namaskara');
 })
+app.get("/exercise-2", (req, res) => {
+    res.redirect("/exercise-2-redirected");
+})
+app.get("/exercise-2-redirected", (req, res) => {
+    res.send("REDIRECTED")
+})
 app.listen(port, () => { console.log("Server is running") })
