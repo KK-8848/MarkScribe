3b4451eb73a7f17bc8ba6bab486423fdeb44eba2
KK-8848 <karthikkrishna8848@gmail.com>
2024-09-16 15:11:25 +0530

    Added the semantics ejs

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get("/exercise-2", (req, res) => {
     res.redirect("/exercise-2-redirected");
 })
 app.get("/exercise-2-redirected", (req, res) => {
-    res.send("REDIRECTED")
+    res.send("/exercise-2.ejs")
 })
 app.listen(port, () => { console.log("Server is running") })
