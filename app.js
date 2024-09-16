31e144bb94d77a478d8cb6fee6b9585aeb5acf08
KK-8848 <karthikkrishna8848@gmail.com>
2024-09-16 16:00:37 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
     res.render('namaskara');
@@ -10,6 +11,16 @@ app.get("/exercise-2", (req, res) => {
     res.redirect("/exercise-2-redirected");
 })
 app.get("/exercise-2-redirected", (req, res) => {
-    res.render('exercise-2.ejs')
+    res.render('exercise-2.ejs');
 })
+app.get("/exercise-3", (req, res) => {
+    res.render('exercise-3.ejs');
+})
+app.get('/result', (req, res) => {
+    const query = req.query.string;
+
+
+    res.render('result.ejs', { query });
+});
+
 app.listen(port, () => { console.log("Server is running") })
