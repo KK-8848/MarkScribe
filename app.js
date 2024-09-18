2306000f5d4c90d7dfb61220dc58e0e59c0f3906
KK-8848 <karthikkrishna8848@gmail.com>
2024-09-18 13:35:38 +0530

    Exercise-5 first commit

--- app.js
+++ app.js
@@ -2,30 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3007;
 const fs = require("fs");
-const marked = require('marked');
-
-const showdown = require('showdown');
-
-const markdownString = "< person > K  </person>";
-const converter = new showdown.Converter();
-const html = converter.makeHtml(markdownString);
-console.log(html)
-
-if (html !== '') { // or check for specific HTML tags/structure
-    console.log('The string is in Markdown format');
-} else {
-    console.log('The string is not in Markdown format');
-}
-const markdownIt = require('markdown-it');
-const md = markdownIt();
-
-const markdownText = "<person>K</person>";
-const parsedHtml = md.render(markdownText);
-console.log(!md);
-console.log(parsedHtml);
-
-// Validate the parsed HTML against a schema or expected output
-// For example, ensure the HTML has a specific structure or contains certain elements
+const path = require("path");
 const readlineSync = require('readline-sync');
 let data = "";
 app.use(require('express-useragent').express());
@@ -33,7 +10,11 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }))
 app.set('trust proxy', true);
 
+function dateParse(dateTime) {
+    const date = dateTime[0].split("/").join("");
+    const time = dateTime[1].split(":").join("")
 
+}
 app.get("/", (req, res) => {
     res.render('namaskara');
 })
@@ -105,7 +86,31 @@ app.post('/exercise-4/result', (req, res) => {
 app.get("/exercise-4/result", (req, res) => {
     res.render('search.ejs', { query, lines: [], pageno: 1, totalPages: 1 })
 })
+app.get("/exercise-5", (req, res) => {
+    res.render('exercise-5.ejs');
+})
+app.post("/exercise-5/post", (req, res) => {
+    const content = req.body.string;
+    if (!content) {
+        res.redirect('/exercise-5');
+    }
 
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+    const day = String(date.getDate()).padStart(2, '0');
+    const hour = String(date.getHours()).padStart(2, '0');
+    const minute = String(date.getMinutes()).padStart(2, '0');
+    const second = String(date.getSeconds()).padStart(2, '0');
+    const filename = `${year}${month}${day}${hour}${minute}${second}.md`
+    const filepath = path.join(__dirname, "markdownfiles", filename);
+    console.log(filepath);
+    fs.writeFile(filepath, content, (err) => {
+        if (err) throw err;
+
+    })
+    res.render("saved.ejs", { filename });
+})
 
 
 app.listen(port, () => { console.log("Server is running") })
