2b9f7c99d55da2e78aa81db085307ba370b4de0a
KK-8848 <karthikkrishna8848@gmail.com>
2024-09-18 15:28:48 +0530

    Added text area

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.post("/exercise-5/submission", (req, res) => {
 
     const date = new Date();
     const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+    const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
     const hour = String(date.getHours()).padStart(2, '0');
     const minute = String(date.getMinutes()).padStart(2, '0');
